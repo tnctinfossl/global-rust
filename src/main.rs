e93cc4204202aba13dcc27bb3b3314279e672fe7
terurin <hamusutaateru@live.jp>
2019-09-12 17:04:53 +0900

    read settings

--- src/main.rs
+++ src/main.rs
@@ -3,34 +3,54 @@ mod subprograms;
 
 use std::env;
 use std::fs::File;
-use std::io::BufWriter;
-use subprograms::{SubProgram,SubPrograms,SubProgramsTrait};
+use std::io::{BufWriter,BufReader};
+use subprograms::{SubProgram, SubPrograms, SubProgramsTrait};
+use settings::Settings;
 
 fn main() {
-    let subprograms: SubPrograms = vec![SubProgram::new(
-        "init".to_string(),
-        "[filename]".to_string(),
-        "create settings file".to_string(),
-        Box::new(init))
-    ];
-
     let args: Vec<String> = env::args().collect();
+    let subprograms: SubPrograms = vec![
+        SubProgram::new(
+            "init".to_string(),
+            "[filename]".to_string(),
+            "create settings file".to_string(),
+            Box::new(init),
+        ),
+        SubProgram::new(
+            "receive".to_string(),
+            "[filename]".to_string(),
+            "receive test from servers".to_string(),
+            Box::new(receive),
+        ),
+    ];
 
     subprograms.run(&args[1..]);
 }
 
 fn init(args: &[String]) {
     //TODO きれいに書く
-    let filename: &str = if let Some(name) = args.iter().nth(2) {
+    let filename: &str = if let Some(name) = args.iter().nth(1) {
         name
     } else {
         "setting.json"
     };
 
-    let file = File::create(filename).expect("Error:Create Setting file");
+    let file = File::create(filename).expect(&format!("Error:Cannot Create {}",filename));
     let writer = BufWriter::new(file);
-    let default = settings::Settings::default();
-    serde_json::to_writer(writer, &default).expect("Error:Write Json");
+    let default = Settings::default();
+    serde_json::to_writer(writer, &default).expect(&format!("Error:Cannot Write {}",filename));
 }
 
-fn receive(args: &[String]) {}
+fn receive(args: &[String]) {
+    //simple receiver
+    let filename: &str = if let Some(name) = args.iter().nth(1) {
+        name
+    } else {
+        "setting.json"
+    };
+
+    let file = File::open(filename).expect(&format!("Error:Cannot Read {}",filename));
+    let reader = BufReader::new(file);
+    let mut json:Settings=serde_json::from_reader(reader).expect(&format!("Error:Cannot Parse {}",filename));
+    println!("{:?}",json);
+}
