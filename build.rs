b9d54b79d75ef41957b546961efdfe30a11e32cf
teru <hamusutaateru@live.jp>
2019-09-17 05:05:55 +0900

    fix build script

--- build.rs
+++ build.rs
@@ -4,15 +4,16 @@ use protoc_rust::Customize;
 use std::error::Error;
 
 fn main ()->Result<(),Box<dyn Error>>{
-    let proto_files=vec!["src/proto/grSim_Replacement.proto"];
-
+    let proto_files=vec!["src/proto/grSim_Packet.proto","src/proto/grSim_Replacement.proto","src/proto/grSim_Commands.proto"];
+    let includes =vec!["src/proto"];
     protoc_rust::run(protoc_rust::Args{
         input:&proto_files[..],
         out_dir:"src/receiver",
-        includes:&[],
+        includes:&includes[..],
         customize: Customize {
             ..Default::default()
         },
     })?;
     Ok(())
 }
+
