b473b3df5ac63974359a4245496cd1796c8aad60
terurin <hamusutaateru@live.jp>
2019-10-10 22:27:10 +0900

    add enumurate

--- model/src/model.rs
+++ model/src/model.rs
@@ -1,81 +1,126 @@
 use glm::Vec2;
+use std::collections::HashMap;
 use std::time;
-#[derive(Debug,Clone,Copy)]
+#[derive(Debug, Clone)]
 pub struct Robot {
     pub id: u32,
     pub position: Vec2,
     pub angle: f32,
-    pub time: time::Instant, //追加する
-    pub confidence:f32,
+    pub time: time::Instant,
+    pub confidence: f32,
+    pub tags: HashMap<String, String>, //追加する
 }
 
 impl Robot {
-    pub fn new(id:u32,position: Vec2,angle:f32,confidence:f32) ->Robot {
+    pub fn new(id: u32, position: Vec2, angle: f32, confidence: f32) -> Robot {
         Robot {
-            id:id,
+            id: id,
             position: position,
-            angle:angle,
+            angle: angle,
             time: time::Instant::now(),
-            confidence:confidence
+            confidence: confidence,
+            tags: HashMap::new(),
         }
     }
     pub fn is_alive(&self, limit: time::Duration) -> bool {
         let d = time::Instant::now() - self.time;
         d < limit
     }
-    pub fn alive(&mut self){
-        self.time=time::Instant::now();
+    pub fn alive(&mut self) {
+        self.time = time::Instant::now();
     }
 }
 
-#[derive(Debug,Clone,Copy)]
+#[derive(Debug, Clone, Copy)]
 pub struct Ball {
     pub position: Vec2,
     pub time: time::Instant, //追加する
-    pub confidence:f32,
+    pub confidence: f32,
 }
 
 impl Ball {
-    pub fn new(position: Vec2,confidence:f32) -> Ball {
+    pub fn new(position: Vec2, confidence: f32) -> Ball {
         Ball {
             position: position,
             time: time::Instant::now(),
-            confidence:confidence
+            confidence: confidence,
         }
     }
     pub fn is_alive(&self, limit: time::Duration) -> bool {
         let d = time::Instant::now() - self.time;
         d < limit
     }
-    pub fn alive(&mut self){
-        self.time=time::Instant::now();
+    pub fn alive(&mut self) {
+        self.time = time::Instant::now();
     }
 }
-#[derive(Debug,Clone)]
-pub struct Team{
-    pub robots:Vec<Box<Robot>>
-
+#[derive(Debug, Clone)]
+pub struct Team {
+    pub robots: Vec<Box<Robot>>,
+    pub name: String,
+    pub score: u32,
+    pub red_card: u32,
+    pub yellow_card: u32,
+    pub goalie: u32,
 }
 
-impl Default for Team{
-    fn default()->Team{
-        Team{
-            robots:vec![]
+impl Default for Team {
+    fn default() -> Team {
+        Team {
+            robots: vec![],
+            name: "unknown".to_owned(),
+            score: 0,
+            red_card: 0,
+            yellow_card: 0,
+            goalie: 0,
         }
     }
 }
 
+#[derive(Debug, Clone, Copy)]
+pub enum TeamColor {
+    Blue,
+    Yellow,
+}
 
+#[derive(Debug, Clone, Copy)]
+pub enum Command {
+    Halt,
+    Stop,
+    NormalStart,
+    ForceStart,
+    PrepareKickOff(TeamColor),
+    PreparePenalty(TeamColor),
+    DirectFree(TeamColor),
+    IndirectFree(TeamColor),
+    Timeout(TeamColor),
+}
 
+#[derive(Debug, Clone, Copy)]
+pub enum Stage {
+    NormalFirstHalfPre,
+    NormalFirstHalf,
+    NormalHalfTime,
+    NormalSecondHalfPre,
+    NormalSecondHalf,
+    ExtraTimeBreak,
+    ExtraFirstHalfPre,
+    ExtraFirstHalf,
+    ExtraHalfTime,
+    ExtraSecondHalfPre,
+    ExtraSecondHalf,
+    PenaltyShootoutBreak,
+    PenaltyShootout,
+    PostGame,
+}
 
-
-
-
-#[derive(Debug,Clone)]
+#[derive(Debug, Clone)]
 pub struct World {
     pub balls: Vec<Box<Ball>>,
     pub blues: Team,
     pub yellows: Team,
+    pub command: Option<Command>,
+    pub stage:Option<Stage>,
 }
 
 impl Default for World {
@@ -84,6 +129,8 @@ impl Default for World {
             balls: vec![],
             blues: Team::default(),
             yellows: Team::default(),
+            command: None,
+            stage:None,
         }
     }
 }
