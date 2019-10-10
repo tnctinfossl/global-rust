02c20e17a202285419eb33a4f08b987618b497ce
terurin <hamusutaateru@live.jp>
2019-10-11 00:48:30 +0900

    edit glade

--- viewer/src/main.rs
+++ viewer/src/main.rs
@@ -1,4 +1,12 @@
-
+extern crate model;
+use std::sync::{Arc, RwLock};
+mod viewer;
+mod field;
+mod size_mode;
+mod fps_counter;
 fn main() {
-    
+    let world = Arc::new(RwLock::new(model::World::default()));
+    let settings = viewer::Settings::default();
+    let window = viewer::Viewer::new(&settings, world).unwrap();
+    window.run();
 }
