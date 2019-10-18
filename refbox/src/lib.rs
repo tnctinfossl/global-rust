aa832a52a38f159c590b4f08387e6f483d1f36d9
terurin <hamusutaateru@live.jp>
2019-10-18 15:29:21 +0900

    channeled refbox

--- refbox/src/lib.rs
+++ refbox/src/lib.rs
@@ -1,6 +1,5 @@
 mod game_event;
 mod referee;
-mod updater;
 pub mod refbox;
 pub use refbox::{RefBox,Settings};
 extern crate model;
--- refbox/src/main.rs
+++ refbox/src/main.rs
@@ -2,11 +2,11 @@ mod refbox;
 use std::sync::{Arc,RwLock};
 mod referee;
 mod game_event;
-mod updater;
 extern crate protobuf;
 extern crate model;
 fn main() {
-    let world = Arc::new(RwLock::new(model::World::default()));
-    refbox::RefBox::spawn(&refbox::Settings::default(), world).unwrap();
-    loop{}
+    let reciver=refbox::RefBox::spawn(&refbox::Settings::default()).unwrap();
+    loop{
+        println!("{:?}",reciver.recv().unwrap());
+    }
 }
