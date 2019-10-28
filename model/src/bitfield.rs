a6ad89baf39f43a5b782dc6b99f962ef073f4cf0
terurin <hamusutaateru@live.jp>
2019-10-28 14:34:14 +0900

    u128で作成した

--- model/src/bitfield.rs
+++ model/src/bitfield.rs
@@ -1,193 +1,116 @@
 use std::ops::*;
-/*
-* 32x16[bit]によって構成される。
-*/
-#[derive(Debug, Clone, Copy)]
-pub struct BitFiled {
-    bits: [u16; 32],
+use std::u128;
+#[derive(Debug, Default, Clone, Copy, Hash, PartialEq)]
+pub struct BitField {
+    pub field: u128,
+    //メモ 縦*横
 }
+
 #[allow(dead_code)]
-impl BitFiled {
-    pub const fn new() -> BitFiled {
-        BitFiled { bits: [0; 32] }
+impl BitField {
+    pub fn new() -> BitField {
+        Self::default()
     }
-    pub const fn height() -> usize {
+
+    pub fn from_u128(x: u128) -> BitField {
+        Self { field: x }
+    }
+
+    pub fn height() -> usize {
+        8
+    }
+
+    pub fn height_log2() -> usize {
+        3
+    }
+
+    pub fn width() -> usize {
         16
     }
-    pub const fn width() -> usize {
-        32
-    }
-    pub const fn position((x, y): (usize, usize)) -> BitFiled {
-        BitFiled {
-            bits: {
-                let mut bits = [0; 32];
-                bits[x] |= 1 << y;
-                bits
-            },
-        }
-    }
-    pub fn mask_right() -> BitFiled {
-        BitFiled {
-            bits: {
-                let mut bits = [0; 32];
-                for index in 0..16 {
-                    bits[index] = u16::max_value()
-                }
-                bits
-            },
-        }
-    }
-    pub fn mask_left() -> BitFiled {
-        BitFiled {
-            bits: {
-                let mut bits = [0; 32];
-                for index in 16..32 {
-                    bits[index] = u16::max_value()
-                }
-                bits
-            },
-        }
-    }
-    pub fn mask_inside() -> BitFiled {
-        BitFiled {
-            bits: {
-                let mut bits = [0; 32];
-                for index in 1..31 {
-                    bits[index] = 0x7ffe;
-                }
-                bits
-            },
-        }
-    }
-    pub fn mask_outside() -> BitFiled {
-        BitFiled {
-            bits: {
-                let mut bits = [0; 32];
-                bits[0] = 0xffff;
-                for index in 1..31 {
-                    bits[index] = 0x8001;
-                }
-                bits[31] = 0xffff;
-                bits
-            },
-        }
-    }
-    pub fn exist(&self) -> bool {
-        self.bits.iter().map(|x| *x != 0).fold(false, |x, y| x | y)
+
+    pub fn width_log2() -> usize {
+        4
     }
 
-    pub fn count_ones(self) -> u32 {
-        self.bits.iter().map(|x| x.count_ones()).sum()
+    fn index(x: usize, y: usize) -> usize {
+        x << Self::height_log2() + y
     }
 
-    pub fn count_zeros(self) -> u32 {
-        self.bits.iter().map(|x| x.count_zeros()).sum()
+    pub fn new_position(x: usize, y: usize) -> BitField {
+        Self::from_u128(1 << BitField::index(x, y))
     }
 
-    pub fn set(&mut self, (x, y): (usize, usize)) -> &mut Self {
-        self.bits[y] |= 1 << x;
-        self
+    pub fn get(&self, x: usize, y: usize) -> bool {
+        (self.field & (1 << BitField::index(x, y))) != 0
     }
 
-    pub fn clear(&mut self, (x, y): (usize, usize)) -> &mut Self {
-        self.bits[y] &= !(1 << x);
-        self
+    pub fn is_zero(&self) -> bool {
+        self.field == 0
     }
 
-    pub fn set_value(&mut self, (x, y): (usize, usize), value: bool) -> &mut Self {
-        if value {
-            self.set((x, y))
-        } else {
-            self.clear((x, y))
-        }
+    pub fn new_right()->BitField{
+        let right = 0xffffffff_ffffffff;
+        Self::from_u128(right)
     }
+
+    pub fn new_left()->BitField{
+        !(BitField::new_right())
+    }
+
     
-    pub fn get(&self, (x, y): (usize, usize)) -> bool {
-        (self.bits[y]&(1<<x))!=0
-    }
 
 }
 
-impl BitOr for BitFiled {
-    type Output = BitFiled;
-    fn bitor(self, rhs: BitFiled) -> Self::Output {
-        BitFiled {
-            bits: {
-                let mut bits = [0; 32];
-                for index in 0..32 {
-                    bits[index] = self.bits[index] | rhs.bits[index];
-                }
-                bits
-            },
-        }
+impl BitAnd for BitField {
+    type Output = BitField;
+    fn bitand(self, rhs: BitField) -> BitField {
+        Self::from_u128(self.field & rhs.field)
     }
 }
 
-impl BitAnd for BitFiled {
-    type Output = BitFiled;
-    fn bitand(self, rhs: BitFiled) -> Self::Output {
-        BitFiled {
-            bits: {
-                let mut bits = [0; 32];
-                for index in 0..32 {
-                    bits[index] = self.bits[index] & rhs.bits[index];
-                }
-                bits
-            },
-        }
+impl BitOr for BitField {
+    type Output = BitField;
+    fn bitor(self, rhs: BitField) -> BitField {
+        Self::from_u128(self.field | rhs.field)
     }
 }
 
-impl BitXor for BitFiled {
-    type Output = BitFiled;
-    fn bitxor(self, rhs: BitFiled) -> Self::Output {
-        BitFiled {
-            bits: {
-                let mut bits = [0; 32];
-                for index in 0..32 {
-                    bits[index] = self.bits[index] ^ rhs.bits[index];
-                }
-                bits
-            },
-        }
+impl BitXor for BitField {
+    type Output = BitField;
+    fn bitxor(self, rhs: BitField) -> BitField {
+        Self::from_u128(self.field ^ rhs.field)
     }
 }
 
-impl BitOrAssign for BitFiled {
-    fn bitor_assign(&mut self, rhs: BitFiled) {
-        for index in 0..32 {
-            self.bits[index] |= rhs.bits[index];
-        }
+impl Not for BitField {
+    type Output = BitField;
+    fn not(self) -> BitField {
+        Self::from_u128(!self.field)
     }
 }
 
-impl BitAndAssign for BitFiled {
-    fn bitand_assign(&mut self, rhs: BitFiled) {
-        for index in 0..32 {
-            self.bits[index] &= rhs.bits[index];
-        }
+impl BitAndAssign for BitField {
+    fn bitand_assign(&mut self, rhs: BitField) {
+        self.field &= rhs.field;
     }
 }
 
-impl BitXorAssign for BitFiled {
-    fn bitxor_assign(&mut self, rhs: BitFiled) {
-        for index in 0..32 {
-            self.bits[index] ^= rhs.bits[index];
-        }
+impl BitOrAssign for BitField {
+    fn bitor_assign(&mut self, rhs: BitField) {
+        self.field |= rhs.field;
     }
 }
 
-impl Neg for BitFiled {
-    type Output = BitFiled;
-    fn neg(self) -> BitFiled {
-        BitFiled {
-            bits: {
-                let mut bits = [0; 32];
-                for index in 0..32 {
-                    bits[index] = !self.bits[index];
-                }
-                bits
-            },
-        }
+impl BitXorAssign for BitField {
+    fn bitxor_assign(&mut self, rhs: BitField) {
+        self.field ^= rhs.field;
     }
 }
+
+impl Deref for BitField {
+    type Target = u128;
+    fn deref(&self) -> &Self::Target {
+        &self.field
+    }
+}
+
