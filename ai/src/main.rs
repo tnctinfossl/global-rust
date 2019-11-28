7aa442d296c692b7e998cd42e17aead498209973
teru <hamusutaateru@live.jp>
2019-11-29 08:18:24 +0900

    実験用環境

--- ai/src/main.rs
+++ ai/src/main.rs
@@ -10,11 +10,15 @@ use geometry::*;
 use std::time::{Duration, Instant};
 use rand::{Rng, SeedableRng};
 fn main() {
-    let mut rng = rand_xoshiro::Xoshiro256StarStar::seed_from_u64(123);
-    let mut world=model::World::new();
-    world.alocate_random( &mut rng, 10);
+    //let mut rng = rand_xoshiro::Xoshiro256StarStar::seed_from_u64(123);
+    //let mut world=model::World::new();
+    //world.alocate_random( &mut rng, 10);
+    //println!("{:?}",evaluation::evaluate_shoot(&world.field, &world.blues, &world.yellows));
+    let begin=vec2(0.0,0.0);
+    let end=vec2(1.0,0.0);
+    let objects=[vec2(0.5,1.0)];
+    println!("{}",evaluation::passable((begin,end), &objects));
 
-    println!("{:?}",evaluation::evaluate_shoot(&world.field, &world.blues, &world.yellows));
 }
 
 
