c424766635ce2513b1c7b4e22b0ac610cfd4e764
sim0822 <Rsim08@outlook.jp>
2019-12-05 02:08:17 +0900

    途中かけ

---
+++ ai/src/game.rs
@@ -0,0 +1,60 @@
+use glm::*;
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::collections::VecDeque;
+
+#[derive(Debug,Clone,Copy)]
+pub struct Robot{
+    id:u32,
+    position:Vec2,
+    angle:f32//rad
+}
+
+impl Default for Robot{
+    #[allow(dead_code)]
+    fn default() ->Robot{
+        Robot{
+            id:0,
+            position:Vec2::new(0.0,0.0),
+            angle:0.0
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Ball{
+    position: Vec2,
+}
+
+impl Default for Ball{
+    #[allow(dead_code)]
+    fn default()->{
+        position:Vec2::new(0.0,0.0)
+    }
+}
+
+
+#[derive(Debug,Clone)]
+pub struct Scene{
+    mines:Vec<Robot>,
+    enemies:Vec<Robot>,
+    ball:Vec<Ball>
+}
+
+impl Default for Scene{
+    #[allow(dead_code)]
+    fn default() ->Scene{
+        Scene{
+            mines:vec![],
+            enemies:vec![],
+            ball:vec![]
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct History{
+    scenes:VecDeque<Rc<RefCell<Scene>>>,
+    score:(f32,f32),
+    //futures:Vec<>
+}
--- ai/src/tree.rs
+++ ai/src/tree.rs
@@ -4,24 +4,28 @@ use super::evaluation::*;
 use glm::*;
 use model::*;
 use std::iter::*;
+use rand::Rng;
+use rand_distr::{Normal,Distribution};
 
 /*pub fn tree(world:&World){
-
+    world.
     //team交互に切り替え
-    (..).map(tree_placement()).collect();
-    tree_evalution(serch_renge());
-    sort;
 
-}*/
+}
 
-//fn tree_placement(team:&Team)->{}
+fn tree_placement<R:Rng>(rng: &mut R,team:&Team)->Vec2{
+    let mut count = 0.0;
+    let mut time = count * 0.001;//1ms
+    let fastest_v = 10.0;
+    let mut prev_position = Vec2::new(0.0,0.0);
+    let current_position =  &team.robots.;
+    let mut v = current_position - prev_position; 
+    let q = Normal::new(current_position - v*time,fastest_v / time);
+    count += 1.0;
+}
 
 fn tree_evalution(world:&World)->(f32,f32){
 
     
     space_domination(&world.blues,&world.yellows,&world.field)
-}
-
-/*fn serch_renge(prev_position:Vec2,current_position:Vec2)->{
-
 }*/
