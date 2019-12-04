c77d106f04c2c370a94bd1861d86b304f8077b98
sim0822 <Rsim08@outlook.jp>
2019-12-04 21:04:23 +0900

    形だけ

--- ai/src/tree.rs
+++ ai/src/tree.rs
@@ -1,8 +1,27 @@
 extern crate model;
 use super::bitfield::BitField;
+use super::evaluation::*;
 use glm::*;
 use model::*;
+use std::iter::*;
 
-pub fn tree(bitfield:&BitField,){
-    //let current_position =  
+/*pub fn tree(world:&World){
+
+    //team交互に切り替え
+    (..).map(tree_placement()).collect();
+    tree_evalution(serch_renge());
+    sort;
+
+}*/
+
+//fn tree_placement(team:&Team)->{}
+
+fn tree_evalution(world:&World)->(f32,f32){
+
+    
+    space_domination(&world.blues,&world.yellows,&world.field)
 }
+
+/*fn serch_renge(prev_position:Vec2,current_position:Vec2)->{
+
+}*/
