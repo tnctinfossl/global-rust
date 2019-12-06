255fa8ef5f528cab27fb7e4337e3ad0a64a1db5c
sim0822 <Rsim08@outlook.jp>
2019-12-07 02:01:30 +0900

    Co-authored-by: terurin <hamusutaateru@live.jp>

--- ai/src/game.rs
+++ ai/src/game.rs
@@ -99,7 +99,7 @@ const HistoryDepth: usize = 4;
 
 #[derive(Debug, Clone)]
 pub struct History {
-    elapsed_time: f32, //非ゼロ
+    period: f32, //非ゼロ
     scenes: [Rc<RefCell<Scene>>; HistoryDepth],
 }
 
@@ -110,7 +110,7 @@ impl History {
             None
         } else {
             Some(History {
-                elapsed_time: period,
+                period: period,
                 scenes: scenes,
             })
         }
@@ -129,30 +129,86 @@ impl History {
         }
     }
 
-    pub fn position(&self,era:usize,id:RobotID)->Option<(Vec2,f32)>{
-        if let Some(r)=self.find(era,id){
+    #[allow(dead_code)]
+    pub fn position(&self, id: RobotID) -> Option<(Vec2, f32)> {
+        if let Some(r) = self.find(0, id) {
             Some((r.position, r.angle))
-        }else{
+        } else {
             None
         }
     }
-    //TODO 時間の概念
-    pub fn velocity(&self,id: RobotID) -> Option<f32> {
-        if self.elapsed_time > 0.0{
-            let v = distance(self.position(0,id).unwrap().0,self.position(1,id).unwrap().0)  / self.elapsed_time;
-            Some(v)
-        }else{
-            None
+    //[0..2PI]の範囲に入れる
+    fn rad_range(x: f32) -> f32 {
+        let two_pi = 2.0 * std::f32::consts::PI;
+        if x < 0.0 {
+            (x % two_pi) + two_pi
+        } else {
+            x % two_pi
         }
+    }
+
+    #[inline(always)]
+    fn rad_diff(now: f32, last: f32) -> f32 {
+        let pi = std::f32::consts::PI;
+        let two_pi = 2.0 * std::f32::consts::PI;
+        //正規化する[0..2PI]
+        let now = Self::rad_range(now);
+        let last = Self::rad_range(last);
+        //短い経路を選択する
+        if abs(now - last) < (pi+std::f32::EPSILON ){
+            now - last
+        } else {
+            if now>last{
+                now - two_pi - last
+            }else{
+                now + two_pi - last
+            }
+        }
+    }
+    //[x_0 - 2*x_1 + x_2]を求める
+    #[inline(always)]
+    fn rad_diff3(first: f32, second: f32, third: f32) -> f32 {
+        let two_pi = 2.0 * std::f32::consts::PI;
+        //正規化する[0..2PI]
+        let first = Self::rad_range(first);
+        let second = Self::rad_range(second);
+        let third = Self::rad_range(third);
+        //短い経路を求めつつ，[x_0 - 2*x_1 + x_2]を求める
+        Self::rad_diff(first, second) - Self::rad_diff(second, third)
+    }
+
+    #[allow(dead_code)]
+    pub fn velocity(&self, id: RobotID) -> Option<(Vec2, f32)> {
+        //データの取得
+        let now = self.find(0, id)?;
+        let last = self.find(1, id)?;
+        //計算する
+        let speed = (now.position - last.position) / self.period;
+        let omega = Self::rad_diff(now.angle, last.angle) / self.period;
+        Some((speed, omega))
+    }
+
+    #[allow(dead_code)]
+    pub fn acceleration(&self, id: RobotID) -> Option<(Vec2, f32)> {
+        //データの取得
+        let first = self.find(0, id)?;
+        let second = self.find(1, id)?;
+        let third = self.find(2, id)?;
+        //差分方程式を計算する
+        let acc = (first.position - second.position * 2.0 + third.position) / (self.period.powi(3));
+        let alpha = Self::rad_diff3(first.angle, second.angle, third.angle) / (self.period.powi(3));
+        Some((acc, alpha))
+    }
+
+    pub fn jerk(&self, id: RobotID) -> Option<(Vec2, f32)> {
+        //データの取得
+        let first = self.find(0, id)?;
+        let second = self.find(1, id)?;
+        let third = self.find(2, id)?;
+        let forth = self.find(3, id)?;
+        None //TODO 準備中です
     }
 }
-
-
-
-
-/*pub fn acceleration(id: RobotID) -> Option<(Vec2, f32)> {}*/
-
-/*pub fn jerk(id: RobotID) -> Option<(Vec2, f32)> {}*/
 
 #[derive(Debug, Clone)]
 pub struct Future {
@@ -169,4 +225,37 @@ impl Future {
 pub struct Tree {
     histories: History,
     score: (f32, f32),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn eq(a: f32, b: f32) -> bool {
+        abs(a - b) < std::f32::EPSILON * 100.0
+    }
+
+    #[test]
+    fn test_rad_range() {
+        let pi = std::f32::consts::PI;
+        assert!(eq(History::rad_range(0.0),0.0));
+        assert!(eq(History::rad_range(pi), pi));
+        assert!(eq(History::rad_range(3.0 * pi), pi));
+        assert!(eq(History::rad_range(-pi), pi));
+        assert!(eq(History::rad_range(-3.0 * pi), pi));
+    }
+
+    #[test] //cargo test
+    fn test_rad_diff() {
+        let two_pi = 2.0 * std::f32::consts::PI;
+        let pi = std::f32::consts::PI;
+        let pi_2 =std::f32::consts::PI/2.0;
+        let pi_4 =std::f32::consts::PI/4.0;
+
+        assert!(eq(History::rad_diff(0.0, 0.0),0.0));
+        assert!(eq(History::rad_diff(pi_2,0.0),pi_2));
+        assert!(eq(History::rad_diff(0.0,pi_2),-pi_2));
+        assert!(eq(History::rad_diff(pi+pi_2,0.0),-pi_2));
+        assert!(eq(History::rad_diff(0.0,pi+pi_2),pi_2));
+    }
 }
