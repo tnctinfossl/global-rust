47423de7d48a81accc7d323d733617a194ea8698
terurin <hamusutaateru@live.jp>
2020-01-07 01:43:50 +0900

    join commit

--- ai/src/lib.rs
+++ ai/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod bitfield;
 pub mod evaluation;
+pub mod formation;
 extern crate glm;
--- ai/src/main.rs
+++ ai/src/main.rs
@@ -3,21 +3,25 @@ use model::*;
 use rand;
 mod bitfield;
 mod evaluation;
+mod formation;
 use evaluation::space_domination;
 use std::time::{Duration, Instant};
 fn main() {
-    let mut world=World::new();
+    let mut world = World::new();
     world.alocate_random(&mut rand::thread_rng(), 8);
     let start = Instant::now();
-    let  (mut a,mut b)=(0.0,0.0);
-    for i in 1..1000{
-        let (c,d)=space_domination(&world); 
-        a+=c;
-        b+=d;
+    let (mut a, mut b) = (0.0, 0.0);
+    for i in 1..1000 {
+        let (c, d) = space_domination(&world);
+        a += c;
+        b += d;
     }
     let end = start.elapsed();
-    println!("{}.{:03}秒経過しました。{}{}", end.as_secs(), end.subsec_nanos() / 1_000_000,a,b);
-
+    println!(
+        "{}.{:03}秒経過しました。{}{}",
+        end.as_secs(),
+        end.subsec_nanos() / 1_000_000,
+        a,
+        b
+    );
 }
-
-
